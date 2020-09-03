1fef8c4aee02ee07ee83ec82d9ae1d1b53711695
Sonu Sharma <sonu.sharma045@gmail.com>
2020-09-03 21:15:43 +0530

    :wheelchair: Create custom loader

---
+++ lib/ui/widgets/g_loader.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_github_connect/ui/theme/export_theme.dart';
+
+class GLoader extends StatelessWidget {
+  final double stroke;
+
+  const GLoader({Key key, this.stroke = 2}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isAndroid
+        ? CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation(GColors.blue),
+            strokeWidth: stroke,
+          )
+        : CupertinoActivityIndicator();
+  }
+}
