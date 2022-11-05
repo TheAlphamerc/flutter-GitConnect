0554fc0f26af8d385a41330bbaf4ce500ce27693
Sonu Sharma <sonu.sharma045@gmail.com>
2022-11-05 10:34:06 +0530

    Upgrade to Android embeddings v2

--- android/app/src/main/java/com/thealphamerc/flutter_github_connect/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.thealphamerc.flutter_github_connect;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/thealphamerc/flutter_github_connect/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thealphamerc.flutter_github_connect.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
